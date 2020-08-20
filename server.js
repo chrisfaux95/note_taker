116257437102a98e82b4ebb0a33f3f5083f41438
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-20 12:54:42 -0400

    fixed display of css

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // IMPORTING NEEDED FUNCTIONS/MODULES
 const express = require("express");
-
+const path = require('path')
 // CREATING THE EXPRESS APP, AND USING A PORT
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,6 +8,8 @@ var PORT = process.env.PORT || 3000;
 // ALLOWING USE OF URL ECODING AND JSON
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+// LINE TO ALLOW USE OF EXTERNAL ASSETS
+app.use(express.static(path.join(__dirname, 'public')))
 
 // ADDING THE SERVER ROUTES
 // ADDING THE SERVER ROUTES
